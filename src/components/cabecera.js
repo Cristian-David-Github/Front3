22883398e854a21979ce9cb4f1c785cc4b914ca7
C.S <c-david94@hotmail.com>
2022-06-15 22:10:52 -0500

    creando estructura de carpetas y componentes f

---
+++ src/components/cabecera.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+// El componente Cabera no tiene componentes hijos.
+// ESTADO: Cabecera no tiene estado.
+// MÉTODOS: Cabecera no tiene métodos.
+// PROPS: Cabecera recibe de su padre la cantidad que va a mostrar dentro del span correpondiente
+// Maqueta de Cabecera:
+//    h1
+//    p > span     (el span mostrará la cantidad recibida por props)
+
+export default function Cabecera({count}) {
+  
+  return (
+    <header>
+        <h1>Shopping Cart</h1>
+        <p>You have selected <span>{count}</span>products</p>
+    </header>
+  )
+}
